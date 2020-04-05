a85fb3ef00a50e35df06a2eada9e08e21f6ec89f
charlotte <hanchaozheng@yahoo.com>
2020-04-05 16:11:44 +0800

    秒杀项目第二章分布式登陆

--- src/main/java/com/imooc/miaosha/controller/GoodsController.java
+++ src/main/java/com/imooc/miaosha/controller/GoodsController.java
@@ -2,23 +2,12 @@ package com.imooc.miaosha.controller;
 
 import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.redis.RedisService;
-import com.imooc.miaosha.result.Result;
-import com.imooc.miaosha.service.MiaoshaUserService;
-import com.imooc.miaosha.service.UserService;
-import com.imooc.miaosha.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 /**
  * 商品列表页
@@ -30,8 +19,8 @@ public class GoodsController {
 
     @Autowired
     RedisService redisService;
-    @Autowired
-    private MiaoshaUserService miaoshaUserService;
+//    @Autowired
+//    private MiaoshaUserService miaoshaUserService;
     private static Logger logger = LoggerFactory.getLogger(GoodsController.class);
 
     /**
